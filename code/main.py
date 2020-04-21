86ff9d707f911c26bbe14454e9eb915dee2e75b2
Laura Verhoeven <l.verhoeven2@students.uu.nl>
2020-04-21 21:37:28 +0200

    added the options of the difficulty (Buttons) to the difficultyScreen

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ class sudoku:
     def __init__(self, parent):
         self.parent = parent
 
-        logo_image = tk.PhotoImage(file="../logo.png")
+        logo_image = tk.PhotoImage(file="../logo/logo.png")
         self.image = logo_image
 
         self.logo_frame = tk.Frame(
@@ -26,6 +26,9 @@ class sudoku:
 
     def homeScreen(self, event):
         self.canvas.delete(tk.ALL)
+        self.difficulty_text.destroy()
+        self.difficulty_options.destroy()
+
         self.logo_frame = tk.Frame(
             self.parent, highlightcolor="white", highlightbackground="white", highlightthickness=3)
         self.logo_frame.grid(row=0, column=0, sticky="",
@@ -35,7 +38,7 @@ class sudoku:
         self.text_frame.grid(row=2, column=2, sticky="", pady=5, padx=5)
         self.text = tk.Label(self.text_frame, text="press spacebar to start", font=(
             "Helvetica", 16), fg="gray")
-        self.text.grid(row=2, column=2, sticky="")
+        self.text.grid(row=2, column=1, sticky="")
 
         self.canvas = tk.Canvas(self.logo_frame, width=350, height=350)
         self.canvas.create_image((0, 10), image=self.image, anchor="nw")
@@ -47,22 +50,40 @@ class sudoku:
             self.text.destroy()
             self.canvas.delete(tk.ALL)
             self.logo_frame = tk.Frame(
-                self.parent)
-            self.logo_frame.grid(row=0, column=0, sticky="nw",
-                                 columnspan=5, rowspan=5)
+                self.parent, width=350, height=125)
+            self.logo_frame.grid(row=0, column=0, sticky="nw")
             self.canvas = tk.Canvas(self.logo_frame, width=350, height=125)
-            # self.canvas = tk.Button(
-            # self.logo_frame, image=self.image, width=350, height=125)
             self.canvas.create_image((0, 0), image=self.image, anchor="nw")
             self.canvas.grid(row=0, column=0, sticky="n")
             self.canvas.bind("<Button-1>", self.homeScreen)
 
             self.difficulty_options = tk.Frame(self.parent)
-            self.difficulty_options.grid(row=1, column=0)
+            self.difficulty_options.grid(row=1, column=1)
 
             self.difficulty_text = tk.Label(
-                self.difficulty_options, text="Pick your difficulty", font=("Helvetica", 22), fg="black")
-            self.difficulty_text.grid(row=1, padx=350)
+                self.difficulty_options, text="Pick your difficulty", font=("Helvetica", 26), fg="black")
+            self.difficulty_text.grid(row=1, column=1, pady=20)
+
+            self.option_very_easy = tk.Button(self.difficulty_options, text="very easy", font=(
+                "Helvetica", 16), fg="black", highlightcolor="gray", highlightbackground="gray", highlightthickness=1, height=2, width=20)
+            self.option_very_easy.grid(row=2, column=1, pady=10)
+
+            self.option_easy = tk.Button(self.difficulty_options, text="easy", font=(
+                "Helvetica", 16), fg="black", highlightcolor="gray", highlightbackground="gray", highlightthickness=1, height=2, width=20)
+            self.option_easy.grid(row=3, column=1, pady=10)
+
+            self.option_medium = tk.Button(self.difficulty_options, text="medium", font=(
+                "Helvetica", 16), fg="black", highlightcolor="gray", highlightbackground="gray", highlightthickness=1, height=2, width=20)
+            self.option_medium.grid(row=4, column=1, pady=10)
+
+            self.options_difficult = tk.Button(self.difficulty_options, text="difficult", font=(
+                "Helvetica", 16), fg="black", highlightcolor="gray", highlightbackground="gray", highlightthickness=1, height=2, width=20)
+            self.options_difficult.grid(row=5, column=1, pady=10)
+
+            self.options_very_difficult = tk.Button(self.difficulty_options, text="very difficult", font=(
+                "Helvetica", 16), fg="black", highlightcolor="gray", highlightbackground="gray", highlightthickness=1, height=2, width=20)
+            self.options_very_difficult.grid(
+                row=6, column=1, pady=10)
 
 
 first = tk.Tk()
